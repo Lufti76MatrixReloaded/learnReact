57362e9a4451309b03d6ea6943ecaad8c987d5d7
André Luft <a.luft1976@gmail.com>
2022-05-17 13:13:21 +0200

    react_new

---
+++ meine-erste-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello World!</h1>
+        <h2>!..The Storm Is Upon Us..!</h2>
+        <h3>§§§-The Truth Is Coming-§§§</h3>
+    </div>
+  );
+}
+
+export default App;
